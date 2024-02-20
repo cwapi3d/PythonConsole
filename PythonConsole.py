faf7779ac58d102fe8e2badafd8e4281443dc485
Michael Brunner <71121348+Brunner246@users.noreply.github.com>
2024-02-20 13:20:45 +0100

    Update PythonConsole

--- PythonConsole.py
+++ PythonConsole.py
@@ -1,7 +1,8 @@
-# Copyright 2020 Cadwork.
+# Copyright 2022 Cadwork.
 # All rights reserved.
 # This file is part of PythonConsole,
 # and is released under the "MIT License Agreement". Please see the LICENSE
 # file that should have been included as part of this package.
 
-import idlelib.idle
+from idlelib.pyshell import main
+main()
